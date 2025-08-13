0bba7e7906f0ae0e004e7ff05217d8b9417a993e
Deepak Sharma <144551642+Deepak9997@users.noreply.github.com>
2025-08-13 14:11:50 +0530

    Updated

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.Frontend_URL || "http://localhost:5173",
+  origin: process.env.Frontend_URL ",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
