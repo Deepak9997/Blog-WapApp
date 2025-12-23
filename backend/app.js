5e64572d5dd26b2c5bc0cbb6b4600785d1460780
Deepak Sharma <144551642+Deepak9997@users.noreply.github.com>
2025-12-23 19:15:42 +0530

    Update CORS origin for the application

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://blog-wapapp.onrender.com',
+  origin: 'https://blog-wapapp-1.onrender.com',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
